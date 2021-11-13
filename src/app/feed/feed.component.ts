0cd81fc7abc6aa09f37e3b202b960e1b047b3d82
Taury <taury.ko@mybit.nl>
2021-11-13 20:43:02 +0100

    before angular update

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { EventService } from "../service/event.service";
 import { RSSParserService } from "../service/feed-parser.service";
+import * as xml2js from "xml2js";
 
 @Component({
-  selector: 'app-feed',
-  templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.less']
+    selector: 'app-feed',
+    templateUrl: './feed.component.html',
+    styleUrls: ['./feed.component.less']
 })
 export class FeedComponent implements OnInit {
     feedLink: string = "";
+    feedResponse: any;
 
-    constructor(private eventService: EventService, private rssParserService: RSSParserService) { 
+    constructor(private eventService: EventService, private rssParserService: RSSParserService) {
     }
 
     ngOnInit(): void {
@@ -25,6 +27,15 @@ export class FeedComponent implements OnInit {
     public getContent(feedLink: string): void {
         this.rssParserService.getRSSFeed(feedLink).subscribe(res => {
             console.log(res);
+            this.getFeedList(res);
+        })
+    }
+
+    public getFeedList(xml: any) {
+        const parser = new xml2js.Parser();
+        parser.parseString(xml, (err:any,result:any) => {
+            console.log(err);
+            console.log(result);
         })
     }
 }
